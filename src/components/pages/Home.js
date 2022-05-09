559da5d1e5a145869587fe5c941b938828c875df
Janelle <janellephalon@gmail.com>
2022-05-08 20:11:48 -0400

    adjusted grid cols

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -43,7 +43,7 @@ export default function Home() {
 
       
       {/* About Me Text */}
-      <p className="about-me-text col-6 text-justify">
+      <p className="about-me-text col-5 text-justify">
       Self-disciplined, enthusiastic full-stack developer with experience in designing and building web applications. Confident implementing a wide range of technologies and programming languages such as HTML, CSS, JavaScript, React, Node, etc. Expert in managing and leading groups of 25+ people. A forever learner who is looking to continue to grow and enhance front/back-end skills.
       </p>
     
