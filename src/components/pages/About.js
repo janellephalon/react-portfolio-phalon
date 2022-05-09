d6e71828238b6f08b69a52dd57eacc4b2d871040
Janelle <janellephalon@gmail.com>
2022-05-09 10:07:37 -0400

    working homepage icons

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,43 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>My Projects</h1>
+      
       <p>
-        
+      <div className="">
+
+{/* Hero Image */}
+<div className="hero-image-secondary">
+
+  <div className="hero-text-secondary">
+    
+
+    <p className="display-1">My Projects</p> 
+
+  </div>
+
+</div>
+
+
+<div className="row">
+
+  {/* Profile Photo */}
+  <div className="profile-photo col-4 img-fluid"></div>
+
+  
+  {/* About Me Text */}
+  <p className="about-me-text col-5 text-justify">
+  Self-disciplined, enthusiastic full-stack developer with experience in designing and building web applications. Confident implementing a wide range of technologies and programming languages such as HTML, CSS, JavaScript, React, Node, etc. Expert in managing and leading groups of 25+ people. A forever learner who is looking to continue to grow and enhance front/back-end skills.
+  </p>
+
+
+</div>
+<footer className="footer">
+<h3>React Portfolio</h3>
+<br></br>
+<h4>Janelle Phalon</h4>
+</footer>
+
+</div>
       </p>
     </div>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,44 +6,39 @@ export default function Home() {
 
     {/* Hero Image */}
     <div className="hero-image">
-
       <div className="hero-text">
         
         <h1 className="display-1">Janelle Phalon</h1>
         <p className="display-4">Full-Stack Web Developer</p> 
 
       </div>
-
     </div>
 
-      {/* Icons */}
-      <div className="icons col-5 container">
+    {/* Icons */}
+    <div className="icons col-5 container">
+      <ul>
 
-<ul>
+        <li className="github">
+          <a href="https://github.com/janellephalon"> <i class="fa-brands fa-github fa-3x github"></i></a>
+        </li>
+        <li className="linkedin">
+          <a href="https://www.linkedin.com/in/janelle-phalon-7b743a157/"> <i class="fa-brands fa-linkedin-in fa-3x linkedin"></i></a>
+        </li>
+        <li className="email">
+          <a href="http://mailto:janellephalon@gmail.com"> <i class="fa-solid fa-at fa-3x email"></i></a>
+        </li>
 
-  <li className="github">
-    <i class="fa-brands fa-github fa-3x"></i> 
-  </li>
-  <li className="linkedin">
-    <i class="fa-brands fa-linkedin-in fa-3x"></i> 
-  </li>
-  <li className="email">
-    <i class="fa-solid fa-at fa-3x"></i> 
-  </li>
-
-</ul>
-
-
-</div>
+      </ul>
+    </div>
   
     <div className="row">
 
       {/* Profile Photo */}
-      <div className="profile-photo col-4 img-fluid"></div>
+      <div className="profile-photo col-5 img-fluid"></div>
 
       
       {/* About Me Text */}
-      <p className="about-me-text col-5 text-justify">
+      <p className="about-me-text col-4 text-justify">
       Self-disciplined, enthusiastic full-stack developer with experience in designing and building web applications. Confident implementing a wide range of technologies and programming languages such as HTML, CSS, JavaScript, React, Node, etc. Expert in managing and leading groups of 25+ people. A forever learner who is looking to continue to grow and enhance front/back-end skills.
       </p>
     
